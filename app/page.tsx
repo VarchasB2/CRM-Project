6ff0db4f74198a0f63c7715c7d079ffd49089bb4
VarchasB2 <myemail@example.com>
2024-07-13 16:48:58 +0530

    speed insights vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import LoginForm from "@/components/login/login-form";
 import Image from "next/image";
 import Login from "./login/page";
 import Dashboard from "./dashboard/page";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function Home() {
   return (
-   <Dashboard/>
+    <>
+      <Dashboard />
+      <SpeedInsights />
+    </>
   );
 }
